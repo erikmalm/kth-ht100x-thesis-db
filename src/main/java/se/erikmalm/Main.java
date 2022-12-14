d1580a28f6d57f8edcc5bbbaaf8c506f3dd4cb7a
erikmalm <70799618+erikmalm@users.noreply.github.com>
2022-12-14 15:30:46 +0100

    working psql drivers

---
+++ src/main/java/se/erikmalm/Main.java
@@ -0,0 +1,42 @@
+package se.erikmalm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println(e);
+            System.exit(-1);
+        }
+
+
+        try {
+            // open connection to database
+            Connection connection = DriverManager.getConnection(
+                    //"jdbc:postgresql://dbhost:port/dbname", "user", "dbpass");
+                    "jdbc:postgresql://localhost:5432/adhoc", "postgres", "admin");
+
+            // build query, here we get info about all databases"
+            String query = "SELECT * FROM public.hallbarheter_privat_id";
+
+            // execute query
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+
+            // return query result
+            while (rs.next())
+                // display table name
+                System.out.println("PostgreSQL Query result: " + rs.getString("id"));
+            connection.close();
+        } catch (java.sql.SQLException e) {
+            System.err.println(e);
+            System.exit(-1);
+        }
+    }
+}
